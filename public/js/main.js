466d7a473a987ddd2aa820309ce5c41386a7bc4b
fperchuk <fperchuk@hotmail.com>
2020-08-07 13:08:29 -0300

    Footer arreglado. Pasaje a arrow functions

--- public/js/main.js
+++ public/js/main.js
@@ -1,49 +1,45 @@
-function listarProyectos(año, materia){
-  var listadoProyectos = document.getElementById('project-content');
+'use strict';
+  const listarProyectos = (año, materia)=>{
   //Lista todos los proyectos de una materia especifica
- db.collection(año).doc(materia).collection("Proyectos").get().then(function(querySnapshot) {
-  console.log(querySnapshot);
-      querySnapshot.forEach(function(doc) {
-          var xmlString = "<div class='col-md-4 col-sm-6 pt-5 "+
-          doc.data().categoria + "'> <a href='proyecto.html?id="+doc.id+"'><div class='single-awesome-project'><img class='img-fluid' src='" +
-          doc.data().imagenes[0] + "' alt=''/><div class='project-dec'><h4>" +
-          doc.data().autor + "</h4></div></div></a></div>";
+  const proyectos = document.getElementById('proyectos');
+  const listadoProyectos = document.createElement("div");
+  listadoProyectos.id='project-content';
+  listadoProyectos.className='row';
+  db.collection(año).doc(materia).collection("Proyectos").get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+          const xmlString = `<div class='col-md-4 col-sm-6 pt-5 ${doc.data().categoria}'><a href='proyecto.html?id=${doc.id}'><div class='single-awesome-project'><img class='img-fluid' src='${doc.data().imagenes[0]}' alt=''/><div class='project-dec'><h4>${doc.data().autor}</h4></div></div></a></div>`;
           listadoProyectos.innerHTML+=xmlString;
-          listadoProyectos.style.height = "";
       });
+      proyectos.appendChild(listadoProyectos);
       mostrarPagina();
   });
-  
-  
-  
 }
 
-function obtenerProyecto(año, materia){
+const obtenerProyecto = (año, materia) => {
 //Obtengo un determinado proyecto
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-if(!urlParams.has('id'))
-{
+if(!urlParams.has('id')){
   window.location.replace("error.html");
 }
 const idProyecto = urlParams.get('id')
-var docRef = db.collection(año).doc(materia).collection("Proyectos").doc(idProyecto);
-var descProy = document.getElementById("descProy");
-var fechaRealizacion = document.getElementById("fechaRealizacion");
-var linkProyecto = document.getElementById("linkProyecto");
-var autor = document.getElementById("autor");
-var tecnologias = document.getElementById("tecnologias");
-var carousel = document.getElementsByClassName("carousel-inner")[0];
+const docRef = db.collection(año).doc(materia).collection("Proyectos").doc(idProyecto);
+const descProy = document.getElementById("descProy");
+const fechaRealizacion = document.getElementById("fechaRealizacion");
+const linkProyecto = document.getElementById("linkProyecto");
+const autor = document.getElementById("autor");
+const tecnologias = document.getElementById("tecnologias");
+const carousel = document.getElementsByClassName("carousel-inner")[0];
 
-docRef.get().then(function(doc) {
+docRef.get().then(doc => {
     if (doc.exists) {
       descProy.innerHTML=doc.data().descripcion;
       fechaRealizacion.innerHTML=doc.data().fechaRealizacion;
       crearIndicadoresCarousel(doc.data().imagenes.length);
-      doc.data().imagenes.forEach(function callback(currentValue, i) {
-        var itemCarousel = document.createElement("div");
+      doc.data().imagenes.forEach((currentValue, i) => {
+        const itemCarousel = document.createElement("div");
         itemCarousel.className="carousel-item";
-        var imgCarousel = document.createElement("img");
+        const imgCarousel = document.createElement("img");
         imgCarousel.className="d-block w-100";
         imgCarousel.src=currentValue;
         if(i==0){
@@ -55,39 +51,36 @@ docRef.get().then(function(doc) {
       });
       linkProyecto.href=doc.data().link;
       autor.innerHTML=doc.data().autor;
-      doc.data().tecnologias.forEach(function callback(currentValue) {
-        var itemTecnologia = document.createElement("li");
+      doc.data().tecnologias.forEach(currentValue => {
+        const itemTecnologia = document.createElement("li");
         itemTecnologia.innerHTML=currentValue;
         tecnologias.appendChild(itemTecnologia);
       });
       mostrarPagina();
     } else {
-        window.location.replace("error.html");
+      window.location.replace("error.html");
     }
-}).catch(function(error) {
+}).catch(error => {
   console.error(error);
   window.location.replace("error.html");
 });
 
 }
 
-function crearIndicadoresCarousel(cantImgs){
-  var indicadorCarousel;
-  var indicadoresCarousel = document.getElementsByClassName("carousel-indicators");
-  for(i=0;i<cantImgs;i++)
-  {
-    indicadorCarousel = document.createElement("li");
+const crearIndicadoresCarousel = cantImgs =>{
+  const indicadoresCarousel = document.getElementsByClassName("carousel-indicators");
+  for(let i=0;i<cantImgs;i++){
+    const indicadorCarousel = document.createElement("li");
     indicadorCarousel.setAttribute('data-target', '#carouselProyecto');
     indicadorCarousel.setAttribute('data-slide-to',i)
     if(i==0){
       indicadorCarousel.className="active";
     }
-    ;
     indicadoresCarousel[0].appendChild(indicadorCarousel);
   }
-  
 }
-function mostrarPagina() {
+
+const mostrarPagina = () => {
   document.getElementById("loader").style.display = "none";
   document.getElementById("contenido").style.display = "block";
 }
